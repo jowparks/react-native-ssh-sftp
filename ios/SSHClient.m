faf7a8e8edb23783ad3f0e495aacb19db090eaf7
Joe <joe@iflabs.network>
2024-03-10 15:28:09 -0700

    update definition nmssh

--- ios/SSHClient.m
+++ ios/SSHClient.m
@@ -15,7 +15,7 @@ int uploadedPerc = 0;
 
 - (instancetype)init {
     if ((self = [super init])) {
-        _session = [[NMSSHSession alloc] init];
+        _session = [[NMSSHSession alloc] initWithHost:@"192.168.1.15" andUsername:@"root"];
         _sftpSession = [[NMSFTP alloc] init];
         _key = [[NSString alloc] init];
         _downloadContinue = false;
